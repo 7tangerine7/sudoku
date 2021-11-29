256f0609658a745f09b048dcd27f0f5738779ae7
Polina <ptukhap@gmail.com>
2021-11-29 20:07:10 +0200

    add init, setup, main

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sudoku",
+    version="1.0",
+    author="Polina",
+    description="._.",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy", "matplotlib"]},
+)
